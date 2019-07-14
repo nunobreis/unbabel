f1ff740d4e0dab5a7d8bace3fe97c186d945316e
Nuno <nuno@Nunos-MacBook-Pro.local>
2019-07-14 19:20:11 +0100

    adds condition isChecked to the reducer

--- src/redux/checkedTranscriptions/checkedTranscriptions.reducer.js
+++ src/redux/checkedTranscriptions/checkedTranscriptions.reducer.js
@@ -9,9 +9,14 @@ const initialState = {}
 export default function checkedTranscriptionsReducer(state = initialState, action) {
   switch (action.type) {
     case CHECKED_TRANSCRIPTION:
+      if (action.transcription.isChecked) {
+        return {
+          ...state,
+          [action.transcription.id]: { ...action.transcription }
+        }
+      }
       return {
-        ...state,
-        [action.transcription.id]: { ...action.transcription }
+        ...state
       }
     case POST_TRANSCRIPTIONS_SUCCESS:
       return {
